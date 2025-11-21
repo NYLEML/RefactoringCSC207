693f712449ba1d0f513cbec6dc8b9915da05861d
Nyle <nyle.hassan@outlook.com>
2025-11-21 00:28:58 -0500

    task 1.1 completeted CheckStyle

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,9 +1,17 @@
 package theater;
 
+/**
+ * Represents a play with a name and a type.
+ *
+ * <p>name and type must never be null
+ *
+ * @null name - never null
+ * @null type - never null
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    String name;
+    String type;
 
     public Play(String name, String type) {
         this.name = name;
--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -8,8 +8,8 @@ import java.util.Map;
  * This class generates a statement for a given invoice of performances.
  */
 public class StatementPrinter {
-    public Invoice invoice;
-    public Map<String, Play> plays;
+    private Invoice invoice;
+    private Map<String, Play> plays;
 
     public StatementPrinter(Invoice invoice, Map<String, Play> plays) {
         this.invoice = invoice;
@@ -24,19 +24,24 @@ public class StatementPrinter {
     public String statement() {
         int totalAmount = 0;
         int volumeCredits = 0;
-        StringBuilder result = new StringBuilder("Statement for " + invoice.getCustomer() + System.lineSeparator());
+        final StringBuilder result =
+                new StringBuilder("Statement for " + invoice.getCustomer() + System.lineSeparator());
 
-        NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
+        final NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
         for (Performance p : invoice.getPerformances()) {
-            Play play = plays.get(p.playID);
+            final Play play = plays.get(p.playID);
 
             int thisAmount = 0;
+            int audienceThreshold = 0;
+
             switch (play.type) {
                 case "tragedy":
-                    thisAmount = 40000;
-                    if (p.audience > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
-                        thisAmount += 1000 * (p.audience - 30);
+                    thisAmount = Constants.TRAGEDY_BASE_AMOUNT;
+                    audienceThreshold = Constants.TRAGEDY_AUDIENCE_THRESHOLD;
+                    if (p.audience > audienceThreshold) {
+                        thisAmount +=
+                                Constants.TRAGEDY_OVER_BASE_CAPACITY_PER_PERSON * (p.audience - audienceThreshold);
                     }
                     break;
                 case "comedy":
@@ -55,13 +60,16 @@ public class StatementPrinter {
             // add volume credits
             volumeCredits += Math.max(p.audience - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
             // add extra credit for every five comedy attendees
-            if ("comedy".equals(play.type)) volumeCredits += p.audience / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
+            if ("comedy".equals(play.type)) {
+                volumeCredits += p.audience / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
+            }
 
             // print line for this order
-            result.append(String.format("  %s: %s (%s seats)%n", play.name, frmt.format(thisAmount / 100), p.audience));
+            result.append(String.format("  %s: %s (%s seats)%n",
+                    play.name, frmt.format(thisAmount / Constants.PERCENT_FACTOR), p.audience));
             totalAmount += thisAmount;
         }
-        result.append(String.format("Amount owed is %s%n", frmt.format(totalAmount / 100)));
+        result.append(String.format("Amount owed is %s%n", frmt.format(totalAmount / Constants.PERCENT_FACTOR)));
         result.append(String.format("You earned %s credits%n", volumeCredits));
         return result.toString();
     }
